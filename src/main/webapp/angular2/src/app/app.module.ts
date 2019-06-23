ba70aa7a92b874448d52dc7c899f64cf10d42127
mightyjava <jeetparmar949@gmail.com>
2019-06-23 16:13:30 +0530

    initial import

---
+++ src/main/webapp/angular2/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
